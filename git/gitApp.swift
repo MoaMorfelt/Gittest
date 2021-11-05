a7f221eee0ef11b222a20cfb023d9c184f4b65fd
Moa Morfelt <moamorfelt@gmail.com>
2021-11-05 10:14:54 +0100

    Initial Commit

---
+++ git/gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitApp.swift
+//  git
+//
+//  Created by Moa Morfelt on 2021-11-05.
+//
+
+import SwiftUI
+
+@main
+struct gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
